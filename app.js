52237a7780f7e6f161707026b7ce0484a3e56aa8
Huong Nguyen <shovity@gmail.com>
2017-12-18 23:17:26 +0700

    initial

---
+++ app.js
@@ -0,0 +1,25 @@
+const http = require('http')
+const express = require('express')
+const path = require('path')
+const bodyParser = require('body-parser')
+
+const index = require('./routes/index')
+
+const app = express()
+const server = http.createServer(app)
+const port = process.env.PORT || 3000
+
+app.set('view engine', 'pug')
+app.set('view cache', false)
+
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.use(index)
+
+server.listen(port)
+
+server.on('listening', () => {
+  console.log(`Server multiple choice test is listening at port ${port}`)
+})
